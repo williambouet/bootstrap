bc8507b025c50ba39038e08ef6a4eb90638568ec
William Bouet <william.bouet.14@gmail.com>
2022-10-19 23:02:23 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary d-flex-sb ">
+    <div class="container-fluid justify-content-between ">
+
+      <a class="navbar-brand" href="#">Discovering Bootstrap 5.1</a>
+
+      <div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse flex-grow-0 px-4 " id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Categories
+            </a>
+            <ul class="dropdown-menu px-1">
+              <li><a class="dropdown-item" href="#">Photos</a></li>
+              <li><a class="dropdown-item" href="#">Articles</a></li>
+              <li><a class="dropdown-item" href="#">Projects</a></li>
+            </ul>
+          </li>
+
+        </ul>
+
+      </div>
+    </div>
+  </nav>
+
+
+  <main class="container-fluid m-0 p-0 ">
+    <div class="row d-flex justify-content-center">
+
+      <h1 class="col-md-12 text-center mt-5 mb-3 container-fluid">Journey insto Bootstrap classes</h1>
+      <div class="card col-xs-1 col-md-6 col-lg-3 shadow p-0 mb-5 bg-white rounded border-0 m-2" style="width: 18rem;">
+        <img src="https://picsum.photos/200/100" class="card-img-top " alt="picsum image">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text d-none d-md-none d-lg-block">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <div class="d-grid gap-2">
+            <a href="#" class="btn btn-primary fs-6 fw-bold p-1">Read more</a>
+          </div>
+        </div>
+      </div>
+      <div class="card col-xs-1 col-md-6 col-lg-3 shadow p-0 mb-5 bg-white rounded border-0 m-2" style="width: 18rem;">
+        <img src="https://picsum.photos/200/100" class="card-img-top img" alt="picsum image">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text d-none d-md-none d-lg-block">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <div class="d-grid gap-2">
+            <a href="#" class="btn btn-primary fs-6 fw-bold p-1">Read more</a>
+          </div>
+        </div>
+      </div>
+      <div class="card col-xs-1 col-md-6 col-lg-3 shadow p-0 mb-5 bg-white rounded border-0 m-2" style="width: 18rem;">
+        <img src="https://picsum.photos/200/100" class="card-img-top img" alt="picsum image">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text d-none d-md-none d-lg-block">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <div class="d-grid gap-2">
+            <a href="#" class="btn btn-primary fs-6 fw-bold p-1">Read more</a>
+          </div>
+        </div>
+      </div>
+      <div class="card col-xs-1 col-md-6 col-lg-3 shadow p-0 mb-5 bg-white rounded border-0 m-2" style="width: 18rem;">
+        <img src="https://picsum.photos/200/100" class="card-img-top img" alt="picsum image">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text d-none d-md-none d-lg-block">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <div class="d-grid gap-2">
+            <a href="#" class="btn btn-primary fs-6 fw-bold p-1">Read more</a>
+          </div>
+        </div>
+      </div>
+      <h2 class="col-md-12 text-center mt-5 container-fluid">The css frameworks are very useful</h2>
+      <h3 class="col-md-12 text-center fs-6 mb-5">Learning them comes with practice</h3>
+    </div>
+  </main>
+
+  <footer class="fixed-bottom">
+    <div class="container-fluid bg-primary text-center fs-6 text-light p-2">
+      © 2022 wilders | images @picsum
+    </div>
+
+  </footer>
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
